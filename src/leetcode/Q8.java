7c755e06658fb0bbc79d7c35c84416383962b068
ztcarrot <273423100@qq.com>
2016-07-23 15:45:40 +0800

    q8

---
+++ src/leetcode/Q8.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+/**
+ * Created by hp2016 on 2016/7/22.
+ */
+public class Q8 {
+    //String to int
+    public static void main (String[] args){
+        System.out.println("hello world");
+    }
+}
